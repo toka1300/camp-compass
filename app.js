2596b44e52a605dcf160857038c7af2ae1eec7f6
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-08-09 09:29:45 -0400

    Error handling completed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
+const {campgroundSchema} = require('./schemas')
 const ExpressError = require('./utilities/ExpressError');
 const catchAsync = require('./utilities/catchAsync');
 
@@ -27,6 +27,16 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const validateCampground = (req, res, next) => {
+    const { error } =  campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', {campgrounds})
@@ -35,21 +45,7 @@ app.get('/campgrounds', catchAsync(async (req, res) => {
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            // price: Joi.location().required(),
-        }).required()
-     })
-    const { error } =  campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400)
-    }
-    console.log('---------->', results, '--------END-----------');
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
@@ -64,8 +60,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground })
 }))
-
-app.put('/campgrounds/:id', catchAsync(async(req, res, next) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res, next) => {
     try {
         const { id } = req.params;
         await Campground.findByIdAndUpdate(id, req.body.campground)
